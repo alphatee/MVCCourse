081ec2c0c47345959303b5ef489e287a56fda30c
Daniel Herrera <dher426@wgu.edu>
2025-03-07 06:58:53 -0800

    Creating a view component

---
+++ WebApp/ViewComponents/TransactionsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    [ViewComponent]
+    public class TransactionsViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "List of Transactions";
+        }
+    }
+}
